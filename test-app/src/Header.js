94fc1aede37f648ef648cf964098ef0f8c0adf3d
Zhanna Lud <zhanna_lud@epam.com>
2017-03-21 13:48:52 +0300

    Added props validation, updated gitignore

--- test-app/src/Header.js
+++ test-app/src/Header.js
@@ -1,6 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 class Header extends Component {
+    static propTypes = {
+        items: PropTypes.array.isRequired,
+        isLoading: PropTypes.bool,
+        submit: PropTypes.func.isRequired,
+        title: PropTypes.string.isRequired,
+        type: PropTypes.oneOf(['news','photos']),
+        user: PropTypes.shape({
+            name: PropTypes.string,
+            age: PropTypes.number
+        }),
+        users: PropTypes.arrayOf(
+            PropTypes.shape({
+                name: PropTypes.string,
+                age: PropTypes.number
+            })
+        )
+    };
     render() {
         console.log(this.props.items)
         return (
