4f3b9563ecec4fb22f34810981b51b49640424da
Zhanna Lud <zhanna_lud@epam.com>
2017-03-22 15:04:12 +0300

    Started with redux. Added simple store and function that tracks states.

--- test-app/src/index.js
+++ test-app/src/index.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
+//
+// ReactDOM.render(
+//     <App />,
+//      document.getElementById('root')
+// );
+import { createStore } from 'redux';
 
-ReactDOM.render(
-    <App />,
-     document.getElementById('root')
-);
+function playList(state = [], action){
+    if (action.type === 'ADD_TRACK'){
+        return[
+            ...state,
+            action.payload
+        ];
+
+    }
+    return state;
+
+}
+
+const store = createStore(playList);
+
+store.subscribe(() => {
+    console.log('subscribe', store.getState());
+
+})
+store.dispatch({type: 'ADD_TRACK', payload: 'Smells like teen spirit'});
+store.dispatch({type: 'ADD_TRACK', payload: 'Enter Sandman'});
